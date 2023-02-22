7ac338b16ad0b6ee010f5a0463f0a9b0b8616bf4
melanimb <92109474+melanimb@users.noreply.github.com>
2023-02-22 13:48:32 +0100

    feat: search container bg img

--- components/SearchbarContainer.jsx
+++ components/SearchbarContainer.jsx
@@ -1,7 +1,7 @@
 const SearchbarContainer = ({ children }) => {
   return (
-    <div className="flex flex-col sm:flex-row justify-center items-center h-96 relative">
-      <h1 className="absolute top-24 text-4xl tracking-wide font-extrabold">FreePics</h1>
+    <div className="flex flex-col sm:flex-row justify-center items-center h-96 relative bg-[url('../public/images/lotus.jpg')] bg-no-repeat bg-cover">
+      <h1 className="absolute top-16 sm:top-24 text-white text-4xl tracking-wide font-extrabold">FreePics</h1>
       {children}
     </div>
   );
