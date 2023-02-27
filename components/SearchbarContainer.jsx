f3796ced3b89b959c4cfe355784b51793ca2b8d7
melanimb <92109474+melanimb@users.noreply.github.com>
2023-02-27 13:43:32 +0100

    feat: mobile sidebar

--- components/SearchbarContainer.jsx
+++ components/SearchbarContainer.jsx
@@ -4,7 +4,7 @@ const SearchbarContainer = ({ children }) => {
   return (
     <>
       <HeroImage />
-      <div className="flex flex-col sm:flex-row justify-center items-center h-[32rem] mt-16 relative left-0 right-0 z-40">
+      <div className="flex flex-col sm:flex-row justify-center items-center h-[32rem] mt-16 relative left-0 right-0 z-30">
         <h1 className="absolute top-28 sm:top-36 text-white text-4xl tracking-wide font-extrabold">
           FreePics
         </h1>
