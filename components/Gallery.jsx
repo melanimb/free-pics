cb65b24b9ec4b3e21d5b51892f46bde71d039000
melanimb <92109474+melanimb@users.noreply.github.com>
2023-03-10 13:09:12 +0100

    fix: layout load imgs
    caused by css columns property, change layout to grid or flex

--- components/Gallery.jsx
+++ components/Gallery.jsx
@@ -1,6 +1,6 @@
 const Gallery = ({ children }) => {
   return (
-    <div className='columns-1 sm:columns-2 lg:columns-3 gap-8'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
       {children}
     </div>
   )
