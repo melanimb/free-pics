bb9b639b788ee5012165701f8211894ae1bdc353
melanimb <92109474+melanimb@users.noreply.github.com>
2023-03-31 15:39:23 +0200

    feat: block scroll modal

--- components/DropdownMenu.jsx
+++ components/DropdownMenu.jsx
@@ -1,10 +1,15 @@
+const getPhoto = () => {
+  console.log('getting photo')
+}
+
 const DropdownMenu = ({ sizes }) => {
   return (
-    <ul className='bg-white shadow-md border border-slate-300 rounded-md absolute right-[11.375rem] top-28'>
+    <ul className='bg-white w-56 shadow-md border border-slate-300 rounded-md absolute top-16 right-0'>
       {sizes.map((size) => (
         <li
           key={size.name}
           className='cursor-pointer py-4 px-6 hover:bg-slate-100'
+          onClick={() => getPhoto()}
         >
           <span className='text-lg font-medium tracking-wide'>{size.name}</span>
           <span className='text-slate-400'>{size.size}</span>
--- components/ImgModal.jsx
+++ components/ImgModal.jsx
@@ -4,6 +4,7 @@ import { faSortDown, faXmark } from '@fortawesome/free-solid-svg-icons'
 import { imgSizes } from 'models'
 import Image from 'next/image'
 import { useState } from 'react'
+import { RemoveScroll } from 'react-remove-scroll'
 import DropdownMenu from './DropdownMenu'
 import Icon from './Icon'
 
@@ -21,32 +22,36 @@ const ImgModal = ({ alt, src, closeModal }) => {
   }
 
   return (
-    <div onClick={() => closeModal()} className='w-screen h-screen fixed top-0 left-0 z-50 bg-black/70'>
-      <article onClick={e => handleArticleClick(e)} className='m-auto w-full md:w-5/6 md:h-[95%] md:mt-6 p-6 bg-white rounded-xl'>
-        <div className='flex justify-between items-center mb-4'>
-          <button onClick={() => closeModal()}>
-            <Icon icon={faXmark} css='w-7 h-7 p-2' />
-          </button>
-          <button
-            className='flex py-3 pl-7 pr-5 rounded-md bg-gradient-to-r from-fuchsia-400 to-blue-500 text-white'
-            onClick={(e) => handleButtonClick(e)}
-          >
-            <p className='text-xl font-medium tracking-wide'>Download</p>
-            <Icon icon={faSortDown} css='w-6 h-6 ml-1 -mt-1' />
-          </button>
-          {showSizes && <DropdownMenu sizes={[imgSizes.ORIGINAL, imgSizes.LARGE, imgSizes.MEDIUM, imgSizes.SMALL]} />}
-        </div>
-        <div className='flex items-center justify-center h-[90%]'>
-          <Image
-            alt={alt}
-            width='500'
-            height='700'
-            src={src}
-            className='rounded-lg'
-          />
-        </div>
-      </article>
-    </div>
+    <RemoveScroll>
+      <div onClick={() => closeModal()} className='w-screen h-screen fixed top-0 left-0 z-50 bg-black/70'>
+        <article onClick={e => handleArticleClick(e)} className='m-auto w-full md:w-5/6 md:h-[95%] md:mt-6 p-6 bg-white rounded-xl'>
+          <div className='flex justify-between items-center mb-4'>
+            <button onClick={() => closeModal()}>
+              <Icon icon={faXmark} css='w-7 h-7 p-2' />
+            </button>
+            <div className='relative'>
+              <button
+                className='flex py-3 pl-7 pr-5 rounded-md bg-gradient-to-r from-fuchsia-400 to-blue-500 text-white'
+                onClick={(e) => handleButtonClick(e)}
+              >
+                <p className='text-xl font-medium tracking-wide'>Download</p>
+                <Icon icon={faSortDown} css='w-6 h-6 ml-1 -mt-1' />
+              </button>
+              {showSizes && <DropdownMenu sizes={[imgSizes.ORIGINAL, imgSizes.LARGE, imgSizes.MEDIUM, imgSizes.SMALL]} />}
+            </div>
+          </div>
+          <div className='flex items-center justify-center h-[90%]'>
+            <Image
+              alt={alt}
+              width='500'
+              height='700'
+              src={src}
+              className='rounded-lg'
+            />
+          </div>
+        </article>
+      </div>
+    </RemoveScroll>
   )
 }
 
