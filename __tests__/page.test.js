cf9238aa41ebad8410e014b754c2b69afcd1f125
melanimb <92109474+melanimb@users.noreply.github.com>
2023-04-18 13:35:54 +0200

    Update page.test.js

--- __tests__/page.test.js
+++ __tests__/page.test.js
@@ -1,13 +1,1 @@
-/* eslint-disable no-undef */
-import { render, screen } from '@testing-library/react'
-import BlurImage from '../components/GalleryContainer'
 
-describe('App', () => {
-  describe('BlurImage', () => {
-    it('renders', () => {
-      render(
-        <BlurImage />
-      )
-    })
-  })
-})
